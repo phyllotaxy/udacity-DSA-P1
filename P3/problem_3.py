02c61156f7e7f14a0cce2e1a0272b621cd64466d
Xiao Wang <phyllotaxy@Xiaos-iMac.local>
2019-06-04 00:23:23 +0200

    Change ordered dict to list in problem 3, finish explanation for problem 3.

--- P3/problem_3.py
+++ P3/problem_3.py
@@ -1,5 +1,4 @@
 import sys
-from collections import OrderedDict
 
 class Node:
 
@@ -23,10 +22,21 @@ class Tree:
         return self.root
 
 def huffman_encoding(data):
-    key_freq = OrderedDict()
+    """
+    Encode the given data and construct a Huffman tree from the data.
+
+    Args:
+      data(str): date to be encoded
+
+    Returns:
+       encoded_data(str): data encoded using huffman encoding
+       huffman_tree(Tree): the huffman_tree that is constructed from data
+    """
+    char_freq = dict()
     for char in data:
-        key_freq[char] = key_freq.get(char, 0) + 1
-    huffman_tree = construct_huffman_tree(key_freq)
+        char_freq[char] = char_freq.get(char, 0) + 1
+    huffman_tree = construct_huffman_tree(char_freq)
+    # traverse the tree and collect the codes in the form of a dictionary
     codes = dict()
     update_codes(huffman_tree.get_root(), codes)
     encoded_data = ''
@@ -35,33 +45,70 @@ def huffman_encoding(data):
     return encoded_data, huffman_tree
 
 def construct_huffman_tree(key_freq):
+    """
+    Construct a Huffman tree.
+
+    Args:
+      char_freq(dict): a dictionary with characters as keys and frequencies as value.
+
+    Returns:
+       huffman_tree(Tree): the huffman_tree that is constructed from the dictionary
+    """
     huffman_tree = Tree()
-    key_freq_sorted = OrderedDict(sorted(key_freq.items(), key=lambda t: t[1]))
-    if len(key_freq_sorted) == 0:
+    # Convert the dictionary into a list that is ordered by value(frequency).
+    key_freq_sorted = sorted(key_freq.items(), key=lambda t: t[1])
+    # If the list is empty (length == 0), return an empty tree.
+    if not key_freq_sorted:
         return huffman_tree
+    # If list length >= 2, we know the first two items has the smallest frequencies,
+    # as the list is ordered by value(frequency).
+    # We always take out two items and put one of type Node back, therefore the
+    # while loop will terminate.
     while len(key_freq_sorted) >= 2:
-        key_1, freq_1 = key_freq_sorted.popitem(last=False)
-        key_2, freq_2 = key_freq_sorted.popitem(last=False)
-        internal_node = Node(freq_1 + freq_2, nodify(trim(key_1)), nodify(trim(key_2)))
-        key_freq_sorted[internal_node] = internal_node.value
-        key_freq_sorted = OrderedDict(sorted(key_freq_sorted.items(), key=lambda t: t[1]))
-    key, _ = key_freq_sorted.popitem()
-    if type(key) is Node:
+        key_1, freq_1 = key_freq_sorted.pop(0)
+        key_2, freq_2 = key_freq_sorted.pop(0)
+        # As the frequency information is retained in the new node by adding
+        # the frequencies of the children, the frequencies of the two child nodes
+        # can be trimmed.
+        internal_node = Node(freq_1 + freq_2, trim(nodify(key_1)), trim(nodify(key_2)))
+        # Insert the item while maintaining the order of the list.
+        for counter, item in enumerate(key_freq_sorted):
+            if internal_node.value <= item[1]:
+                key_freq_sorted.insert(counter, (internal_node, internal_node.value))
+                break
+            elif item == key_freq_sorted[-1]:
+                key_freq_sorted.append((internal_node, internal_node.value))
+                break
+        # Since we have popped two items now, the list could be empty and the for
+        # loop would be skipped, without adding the internal node we created. This
+        # if statement takes care of that special case.
+        if not key_freq_sorted:
+            key_freq_sorted.append((internal_node, internal_node.value))
+
+    # list length == 1 now
+    # Pop the only item out of the list.
+    key, _ = key_freq_sorted.pop()
+    # If the while loop has been entered, the only item's key has to be of type Node.
+    if isinstance(key, Node):
         huffman_tree.root = trim(key)
-    elif type(key) is str:
+    # It could also be the case that the list started out with length == 1, in
+    # which case the only item's key is of type string as the while loop was never entered,
+    # and we need to construct a small Huffman tree with only one root and one leaf.
+    elif isinstance(key, str):
         huffman_tree.root, huffman_tree.root.left = Node(), Node(key)
     return huffman_tree
 
-# Trim the frequency from the node.
-def trim(key):
-    if type(key) is Node and type(key.value) is not str:
-        key.value = None
-    return key
+def trim(node):
+    """Trim the frequency from the node."""
+    if not isinstance(node.value, str):
+        node.value = None
+    return node
 
 def nodify(key):
-    if type(key) is str:
+    """Make the key into a node, if it is not one already."""
+    if isinstance(key, str):
         return Node(key)
-    if type(key) is Node:
+    if isinstance(key, Node):
         return key
     return None
 
@@ -70,21 +117,27 @@ def update_codes(node, codes):
     collect_encodings(node, encoding, codes)
 
 def collect_encodings(node, encoding, codes):
+    """Collect all codes by traversing the Huffman tree."""
     if node.value is None and not (node.has_left_subtree() or node.has_right_subtree()):
         return
     if not (node.has_left_subtree() or node.has_right_subtree()):
+        # Once we reach a leaf node, update the dictionary using the encoding as value.
         codes[node.value] = encoding
         return
     if node.has_left_subtree():
         encoding += '0'
         collect_encodings(node.left, encoding, codes)
+        # Remove the last bit/character after the recursive call.
         encoding = encoding[:-1]
     if node.has_right_subtree():
         encoding += '1'
         collect_encodings(node.right, encoding, codes)
+        # Remove the last bit/character after the recursive call.
         encoding = encoding[:-1]
 
 def huffman_decoding(data, tree):
+    """Collect decoded data by visiting the leaves of the Huffman tree according to the data."""
+    # No need for a recursive function, because we are not really traversing the tree.
     decoded_data = ''
     node = tree.get_root()
     if node.value is None and not (node.has_left_subtree() or node.has_right_subtree()):
@@ -134,8 +187,8 @@ def test(a_great_sentence):
     print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
     print ("The content of the decoded data is: {}\n".format(decoded_data))
 
-# a version of int() that works for empty string
 def mk_int(str, base):
+    """This is a version of int() that works for empty string."""
     return int(str, base) if str else 0
 
 if __name__ == "__main__":
